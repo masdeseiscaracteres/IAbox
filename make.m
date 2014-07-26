254acc403b9b388a72fa4f54bc5b7c05645c7c6a
Óscar González <masdeseiscaracteres@gmail.com>
2014-07-26 13:58:35 +0200

    Use full path when toolbox is added to path

--- make.m
+++ make.m
@@ -2,13 +2,13 @@ function make(command)
 switch lower(command)
     case 'install'
         fprintf('IAbox has been added to search path (until the end of this session)\n');
-        addpath(genpath('example')); % Usage examples
-        addpath(genpath('src')); % Core source code
-        addpath(genpath('test')); % Scripts testing proper functioning of files in "src"
+        addpath(genpath(fullfile(pwd,'example'))); % Usage examples
+        addpath(genpath(fullfile(pwd,'src'))); % Core source code
+        addpath(genpath(fullfile(pwd,'test'))); % Scripts testing proper functioning of files in "src"
     case 'uninstall'
-        rmpath(genpath('example')); % Usage examples
-        rmpath(genpath('src')); % Core source code
-        rmpath(genpath('test')); % Scripts testing proper functioning of files in "src"
+        rmpath(genpath(fullfile(pwd,'example'))); % Usage examples
+        rmpath(genpath(fullfile(pwd,'src'))); % Core source code
+        rmpath(genpath(fullfile(pwd,'test'))); % Scripts testing proper functioning of files in "src"
         fprintf('IAbox has been removed from search path\n');
     case 'packages'
         make install;
